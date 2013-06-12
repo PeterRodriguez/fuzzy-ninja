47127ad00221c5f841a616f24e23facaf44d4a68
PeterRodriguez <peter@aphroditel.eu>
2013-06-12 15:40:19 +0200

    remove dumb migrations

--- db/migrate/20130605103053_add_role_to_clients.rb
+++
@@ -1,5 +0,0 @@
-class AddRoleToClients < ActiveRecord::Migration
-  def change
-    add_column :clients, :role, :string
-  end
-end
--- db/migrate/20130605125356_drop_client_table.rb
+++
@@ -1,8 +0,0 @@
-class DropClientTable < ActiveRecord::Migration
-  def up
-  	drop_table :clients
-  end
-
-  def down
-  end
-end
