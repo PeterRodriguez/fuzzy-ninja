506e231e7fcfc5b08db6fc42b3cd62f85df15a83
PeterRodriguez <peter@aphroditel.eu>
2013-06-12 11:54:13 +0200

    Change tinyint to string

--- db/migrate/20130607103549_add_role_to_user.rb
+++ db/migrate/20130607103549_add_role_to_user.rb
@@ -1,17 +1,17 @@
-class AddSuperadminToUser < ActiveRecord::Migration
+class AddRoleToUser < ActiveRecord::Migration
   def up
-    add_column :users, :superadmin, :boolean, 
+    add_column :users, :role, :string, 
                                     :null => false, 
                                     :default => false 
     User.create! do |r|
       r.email      = 'default@example.com'
       r.password   = 'password'
-      r.superadmin = true
+      r.role = 'admin'
     end
   end
 
   def down
-    remove_column :users, :superadmin
+    remove_column :users, :role
     User.find_by_email('default@example.com').try(:delete)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(:version => 20130607103549) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "email",                  :default => "",  :null => false
+    t.string   "encrypted_password",     :default => "",  :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(:version => 20130607103549) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.boolean  "superadmin",             :default => false, :null => false
+    t.datetime "created_at",                              :null => false
+    t.datetime "updated_at",                              :null => false
+    t.string   "role",                   :default => "0", :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
